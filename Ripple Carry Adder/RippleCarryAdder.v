60992046e911bad12a8e46e87b921e5c1b8c1578
Jai Gora <Jaigora24@gmail.com>
2021-10-03 01:59:56 +0530

    Update RippleCarryAdder.v

--- Ripple Carry Adder/RippleCarryAdder.v
+++ Ripple Carry Adder/RippleCarryAdder.v
@@ -6,9 +6,9 @@ module RippleCarryAdder(A,B,Cin,Sum,Cout);
   
   wire C0,C1,C2;
   
-  Full_Adder FA0(A[0],B[0],Cin,sum[0],C0);
-  Full_Adder FA1(A[1],B[1],C0,sum[1],C1);
-  Full_Adder FA2(A[2],B[2],C1,sum[2],C2);
-  Full_Adder FA03(A[3],B[3],C3,sum[3],Cout);
+  FullAdder FA0(A[0],B[0],Cin,sum[0],C0);
+  FullAdder FA1(A[1],B[1],C0,sum[1],C1);
+  FullAdder FA2(A[2],B[2],C1,sum[2],C2);
+  FullAdder FA03(A[3],B[3],C3,sum[3],Cout);
   
 endmodule
